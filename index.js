439ac397946154ae3b5add6f8c320e5bb8828917
luckymanAM <luckymadhukar9@gmail.com>
2022-04-08 16:51:02 +0530

    First

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const user = {
+    name: 'John',
+    age: 30
+}
+
+app.get('/user', (req, res) => res.send(user));
+
+app.get('/hello', (req, res) => res.send('Hello World on hello route'));
+
+app.listen(port, () => console.log(`Server listening on ${port}!`));
+
